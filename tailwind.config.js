a3713d2a23b69a64604e926a7841faa82e763aa2
fazlarabby1 <fazlarabby41@gmail.com>
2022-10-25 15:51:08 +0600

    created the folder structure and the components

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 }
